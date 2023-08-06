7130874c286c43dddd645e561f9bba5e7eaafa3b
Ibrahim Abu Wasel <i.abuwasel@gmail.com>
2023-08-06 10:42:42 +0300

    few changes

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -41,35 +41,3 @@ def test_add_3(num1, num2, res):
     ])
 def test_add_2(num1, num2, res):
     assert calculator.add(num1, num2) == res
-
-
-#------------------Fixtures----------------------------------#
-
-@pytest.fixture
-def my_fruite():
-    return 'banana'
-
-def test_fruite(my_fruite):
-    assert my_fruite == 'banana'
-
-#-------------------------------------------------------------
-
-#------------------Fixtures with list------------------------#
-@pytest.fixture
-def order():
-    return []
-
-@pytest.fixture
-def append_first(order):
-    order.append(1)
-
-@pytest.fixture
-def append_second(order, append_first):
-    order.extend([2,3,4])
-
-@pytest.fixture(autouse=True)
-def append_third(order, append_second):
-    order += [5]
-
-def test_order(order):
-    assert order == [1, 2, 3, 4, 5]
