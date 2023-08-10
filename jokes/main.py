1a105e78a75b7e1e4c7fc3bb7ad4510dc05275ff
Ibrahim Abu Wasel <i.abuwasel@gmail.com>
2023-08-10 18:26:35 +0300

    few changes

---
+++ jokes/main.py
@@ -0,0 +1,27 @@
+import requests
+
+#url = 'https://api.chucknorris.io/jokes/random'
+url = 'https://api.chucknorris.io/jokes/categories'
+
+def conn_api_and_get_data(url):
+    res = requests.get(url)
+    if res.status_code < 400:
+        data = res.json()
+        return data
+    else:
+        return None
+
+def conn_api_and_get_data_by_query(query):
+    url = (f'https://api.chucknorris.io/jokes/search?query={query}')
+    res = requests.get(url)
+    if res.status_code < 400:
+        data = res.json()
+        return data
+    else:
+        return None
+
+#print(conn_api_and_get_data(url)['id'])
+#print(conn_api_and_get_data(url))
+get_nem_categories = conn_api_and_get_data(url)
+print(conn_api_and_get_data_by_query(get_nem_categories[2]))
+
